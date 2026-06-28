fa65746f6662b2314b15306251de1a7fb2c583be
anujthakare056-sudo <anujthakare056@gmail.com>
2026-06-28 23:03:20 +0530

    Reduce sleep duration in main loop

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,7 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         while(true){
             System.out.println("Hello DevOps Engineers!");
-            Thread.sleep(10000);
+            Thread.sleep(100);
         }
     }
 }
